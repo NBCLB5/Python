de39695efac71f4c8f4b540338e4ea3580145c93
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-02 12:52:42 +0700

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/2/2021
+HelloWorld
+Summary: prints "Hello World! to the console window
+"""
+
+def main(): #definition of the main program
+    print("Hello World!")
+    
+main() # execution of main program

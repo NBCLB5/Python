d44b4c408c61f17a50ec3d923b60b09bf2fc69af
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:05:35 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.2.py
@@ -0,0 +1,11 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.2
+Summary: addition of postive integers
+"""
+
+#Write a program that prints the sum of the first ten positive integers, 1 + 2 + … + 10.
+
+print(1+2+3+4+5+6+7+8+9+10)

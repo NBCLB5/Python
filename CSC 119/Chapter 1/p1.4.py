098e1cf4837670b89addd2117c6963553443c8cc
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:06:06 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.4.py
@@ -0,0 +1,17 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.4
+Summary: prints summary of account after 3 years (without using an algorithm)
+"""
+#Write a program that prints the balance of an account after the first, second, and third year. 
+# The account has an initial balance of $1,000 and earns 5 percent interest per year.
+
+print("Balance of the account")
+print("After the 1st year")
+print("$1050")
+print("After the 2nd year")
+print("1102.5")
+print("After the 3rd year")
+print("$1157.6")

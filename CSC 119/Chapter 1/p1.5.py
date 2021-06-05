08c1b85701a3eb5632a575bf4474b5d469097769
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:06:17 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.5.py
@@ -0,0 +1,12 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.5
+Summary: prints Nathan in a box
+"""
+
+#Write a program that displays your name inside a box on the screen, like this:
+print("+--------+")
+print("| Nathan |")
+print("+--------+")

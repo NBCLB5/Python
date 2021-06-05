e935e5a5d75b35136b7a8b2bddd36cac9b08c22a
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:05:21 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.1.py
@@ -0,0 +1,14 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+P1.1
+Summary: prints greeting in Janpanese and English
+"""
+# Write a program that prints a greeting of your choice, perhaps in a language other than English.
+
+print("こんにちは")
+print("Kon'nichiwa")
+print()
+print("English")
+print("Good Afternoon")

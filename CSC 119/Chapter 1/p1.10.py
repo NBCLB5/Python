8e028e1c58d6c4e5033de3d811053e9a543d5641
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:07:12 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.10.py
@@ -0,0 +1,14 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.10
+Summary: prints an animal speaking a greeting in Vietnamese 
+"""
+#Write a program that prints an animal speaking a greeting,
+
+print("  /\___/\      ----------------    ")
+print(" (  ' '  )    /    xin chào     \  ")
+print(" (   -   )   <        Mới        | ")
+print("  |  |  |     \ người lập trình /  ")
+print(" (___|___)     -----------------   ")

2bfe6dbe0f85424c3d4ef896c77e323c452fb53f
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:07:29 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.11.py
@@ -0,0 +1,12 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.11
+Summary: prints info on spertate lines
+"""
+#Write a program that prints three items, such as the names of your three best friends or favorite movies, on three separate lines.
+
+print("Karl")
+print("TC")
+print("Copper")

c4353f02cc55a8dce5c994a6eb5ce6c34817d9e7
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:06:59 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.9.py
@@ -0,0 +1,16 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.9
+Summary: prints a house
+"""
+#Write a program that prints a house
+
+print("   +   ")
+print("  + +  ")
+print(" +   + ")
+print("+-----+")
+print("| .-. |")
+print("| | | |")
+print("+-+-+-+")

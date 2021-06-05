e1c06cdcc7686f66e156b34af404180c5005c30c
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:05:55 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.3.py
@@ -0,0 +1,10 @@
+
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.3
+Summary:muliplication of postive integers
+"""
+#Write a program that prints the product of the first ten positive integers, 1 × 2 × … × 10. (Use * to indicate multiplication in Python.)
+print(1*2*3*4*5*6*7*8*9*10)

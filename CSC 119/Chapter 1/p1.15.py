3fb1430399d6e74a4464236dde854eb1e3913dd8
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:08:21 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.15.py
@@ -0,0 +1,18 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.15
+Summary: prints State Tax rates for 5 State in the USA
+"""
+#In the United States there is no federal sales tax, so every state may impose its own sales taxes.
+#  Look on the Internet for the sales tax charged in five U.S. states, 
+# then write a program that prints the tax rate for five states of your choice.
+
+print("  Sales Tax Rates  ")
+print("-------------------")
+print(" Colorado   | 4.63%")
+print(" Ohio       | 5.75%")
+print(" Kansas     | 6.50%")
+print(" California | 13.3%")
+print(" Wyoming    | 4.00%")
---
+++ CSC 119/Chapter 1/p1.16.py
@@ -0,0 +1,32 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.16
+Summary: 
+"""
+#The ability to speak more than one language is a valuable skill in today’s labor market. 
+# One of the basic skills is learning to greet people. 
+# Write a program that prints a two-column list with the greeting phrases shown in the following table; 
+# in the first column, print the phrase in English, in the second column, print the phrase in a language of your choice. 
+# If you don’t speak any language other than English, use an online translator or ask a friend.
+"""
+List of Phrases to Translate 
+Good morning. 
+It is a pleasure to meet you. 
+Please call me tomorrow. 
+Have a nice day!
+"""
+print("-------------------------------------------------------------------------")
+print("          Translation table of common words  when traveling              ")
+print("--------------------------------|----------------------------------------")
+print("           English              |                Icelanic                ")
+print("--------------------------------|----------------------------------------")
+print(" Good Morning                   |   Góðan daginn                         ")
+print("-------------------------------------------------------------------------")
+print(" It is a pleasure to meet you.  |   Það er ánægja að kynnast þér         ")
+print("-------------------------------------------------------------------------")
+print(" Please call me tomorrow.       |   Vinsamlegast hringdu í mig á morgun. ")
+print("-------------------------------------------------------------------------")
+print(" Have a nice day!               |   Eigðu góðan dag!                     ")
+print("-------------------------------------------------------------------------")

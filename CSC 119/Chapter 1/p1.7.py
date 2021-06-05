593026cc40519a3efbad61cee7ab6b559288c656
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:06:38 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.7.py
@@ -0,0 +1,16 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.7
+Summary: prints a face using different keyboard charcters
+"""
+# Write a program that prints a face
+
+
+print("  ~~~~~~  ")
+print(" |      | ")
+print("{  D  +  } ")
+print(" |  ..  | ")
+print(" | @#$% | ")
+print(" |______|")

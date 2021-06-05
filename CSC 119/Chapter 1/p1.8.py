d9f75b2a6b0d7e4d43c56d4c6511d63d529df4c4
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:06:49 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.8.py
@@ -0,0 +1,19 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.8
+Summary: prints an imitation of a painting
+"""
+#Write a program that prints an imitation of a Piet Mondrian painting. (Search the Internet if you are not familiar with his paintings.) Use character sequences such as @@@ or ::: to indicate different colors, and use - and | to form lines.
+
+
+print("----------------")
+print("|:::|@@@@@@@@|:|")
+print("|:::|@@@@@@@@|:|")
+print("|---|@@@@@@@@|:|")
+print("|^^^|@@@@@@@@|-|")
+print("|^^^|@@@@@@@@|^|")
+print("|^^^|----------|")
+print("|^^^|::::::::::|")
+print("----------------")

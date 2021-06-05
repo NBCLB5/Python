a448b53113ee62a8ac2e2a0a335cb63874729d1b
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:08:05 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.14.py
@@ -0,0 +1,20 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.14
+Summary: prints a brithday chart of names and dates 
+"""
+#Write a program that prints a two-column list of your friends’ birthdays.
+#  In the first column, print the names of your best friends; in the second column, print their birthdays.
+
+print(    "Birthday chart     ")
+print("-----------------------")
+print("  Name    |    Date    ")
+print(" ---------|------------")
+print("  Karl    |  6/1/2004  ")
+print("   TC     |  2/26/2018 ")
+print("  Toby    |  11/25/2008")
+print("  Copper  |  4/14/1995 ")
+print("  Molly   |  3/11/1991 ")
+print("  Amigo   |  12/5/2016 ")

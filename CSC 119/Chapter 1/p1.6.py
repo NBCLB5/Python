7f775fdbf5b5e9b2488ee2819f59f2dc655bec65
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-06-05 17:06:28 +0700

    Add files via upload

---
+++ CSC 119/Chapter 1/p1.6.py
@@ -0,0 +1,15 @@
+"""
+Nathan Banek
+CSC 119 - section 471
+6/5/2021
+p1.6
+Summary: prints Karl in "*" 
+"""
+#Write a program that prints your name in large letters, such as
+
+print("*     *    **    ****    *")
+print("*   *     *  *   *   *   *")
+print("* *      *    *  ****    *")
+print("*   *    ******  *   *   *")
+print("*     *  *    *  *    *  *****") 
+
